a1ed52a833b3a7369ad38c339cacd551cf3989af
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-06 14:59:56 -0400

    Adding before install script

---
+++ scripts/before_install.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# Install node.js
+sudo apt-get install python-software-properties -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo apt-get update
+sudo apt-get install nodejs -y
