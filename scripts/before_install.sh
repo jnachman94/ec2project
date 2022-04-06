54d536e9adf54aba76028e28234e5818262bf744
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-06 17:36:20 -0400

    new changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-# Install nodemon
-# sudo npm install nodemon -g
+. ~/.nvm/nvm.sh
 
-# Install forever module 
-# https://www.npmjs.com/package/forever
+nvm install node
+
 sudo npm install forever -g
-
-# Clean working folder
-# sudo find /home/ubuntu/test -type f -delete
