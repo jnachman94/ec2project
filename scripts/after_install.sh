6e0c054d17963647e9e6e656e4b5b0adf8043560
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-06 15:07:50 -0400

    Remove before and after install scripts

--- scripts/after_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /home/ec2-user/project
-cd /home/ec2-user/project
-
-npm install
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+mkdir /home/ec2-user/project
+cd /home/ec2-user/project
+
+npm install
 
 npm start
