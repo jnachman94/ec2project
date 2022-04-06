add25bddfbc4ed0de90a021f0f35b267cc449c84
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-06 17:26:36 -0400

    trying to use forever

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,13 @@
 mkdir /home/ec2-user/project
 cd /home/ec2-user/project
 
-sudo ~/.nvm/versions/node/v17.8.0/bin/npm install
+#sudo ~/.nvm/versions/node/v17.8.0/bin/npm install
 
-sudo ~/.nvm/versions/node/v17.8.0/bin/npm start
+#sudo ~/.nvm/versions/node/v17.8.0/bin/npm start
+
+
+#!/bin/bash
+
+# Stop all servers and start the server as a daemon
+forever stopall
+forever start /home/ubuntu/nodejs/app.js
