e0ecfc1c04290ecbafe5a08acc5a7eadc592f226
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-07 11:35:44 -0400

    Move source thing

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
 nvm install node
+
+source /home/ec2-user/.bash_profile
 
 sudo npm install forever -g
