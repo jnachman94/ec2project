c2ae0c8492392d600e38125240935e9bb6a07c90
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-07 13:33:15 -0400

    Fix application start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,10 +5,8 @@ cd /home/ec2-user/project
 #sudo ~/.nvm/versions/node/v17.8.0/bin/npm install
 
 #sudo ~/.nvm/versions/node/v17.8.0/bin/npm start
-
-
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 
 # Stop all servers and start the server as a daemon
 forever stopall
-forever start /home/ubuntu/nodejs/app.js
+forever start /home/ec2-user/project/app.js
