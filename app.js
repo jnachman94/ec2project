ac737352e8cfda3acb331bf53f6e6a6bb5f18f6d
Jacob Nachman <jacobnachman@Jacobs-MacBook-Pro.local>
2022-04-07 15:36:01 -0400

    output to text file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+var counter = 0;
 
 const server = http.createServer((req, res) => {
     console.log('received request!')
+    fs.writeFileSync(path.resolve(__dirname,'output.txt'), `received request number: ${counter++}!\n`, {flag: 'a'});
     res.end("Hello World!");
 });
 
